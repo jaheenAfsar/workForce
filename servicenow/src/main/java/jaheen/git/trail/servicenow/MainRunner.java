98128cd668a556f032457b007e3191fcba7048eb
jaheen.syed <jaheen.syed@servicenow.com>
2017-10-10 17:57:09 +0530

    This is the commit from eclipse

---
+++ servicenow/src/main/java/jaheen/git/trail/servicenow/MainRunner.java
@@ -0,0 +1,12 @@
+package jaheen.git.trail.servicenow;
+
+public class MainRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PrintWriter prt = new PrintWriter();
+		prt.printStarts();
+
+	}
+
+}
---
+++ servicenow/src/main/java/jaheen/git/trail/servicenow/PrintWriter.java
@@ -0,0 +1,20 @@
+package jaheen.git.trail.servicenow;
+
+public class PrintWriter {
+		public void printStarts() 
+		{
+		int i,j,k;
+		for(i=1; i<=5; i++)
+		{
+		for(j=4; j>=i; j--)
+		{
+		System.out.print(" ");
+		}
+		for(k=1; k<=(2*i-1); k++)
+		{
+		System.out.print("*");
+		}
+		System.out.println("");
+		}
+		}
+}
